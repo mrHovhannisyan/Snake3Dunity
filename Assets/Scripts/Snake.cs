e4a3747af19c6227ad5cb48bcf0651e9c3b39782
mrHovhannisyan <karenhovhannisyan01@gmail.com>
2018-04-24 23:13:13 +0400

    Add files via upload
    Movement improved

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Snake : MonoBehaviour
 {
@@ -10,30 +11,31 @@ public class Snake : MonoBehaviour
     public int score;
     public Text TextScore;
 
+    // Speed
+    public float Speed = 1;
     public int RotateSpeed;
 
-    bool ate = false;
-
     public GameObject tailPrefab;
-
     public GenerateFood food;
 
     // Direction to top(forward)
     Vector3 dir = Vector3.forward;
 
-    // Track of Tail
-    List<Transform> tail = new List<Transform>();
+    // List of Tail
+    public List<GameObject> tail = new List<GameObject>();
 
     // Use this for initialization
     void Start()
     {
-        InvokeRepeating("Move", 0, 0.5f);
         food.Generate();
+        tail.Add(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(dir * Speed * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
@@ -44,54 +46,31 @@ public class Snake : MonoBehaviour
         }
     }
 
-    void Move()
+    public void AddTail()
     {
-        // head current position
-        Vector3 v = transform.position;
-
-        transform.Translate(dir);
-
-        // Have eatten something
-        if (ate)
-        {
-            // Load Prefab into the world
-            GameObject g = Instantiate(tailPrefab, v, Quaternion.identity);
-
-            // Keep track of it in our tail list
-            tail.Insert(0, g.transform);
-
-            ate = false;
-        }
-
-        if (tail.Count > 0)
-        {
-			// Give head's current position to tail's last
-            tail.Last().position = v;
-
-            // Add to front of list, remove from the back
-            tail.Insert(0, tail.Last());
-            tail.RemoveAt(tail.Count - 1);
-        }
+        Vector3 newTailPos = tail[tail.Count - 1].transform.position;
+        tail.Add(GameObject.Instantiate(tailPrefab, newTailPos, Quaternion.identity) as GameObject);
     }
 
     void OnTriggerEnter(Collider coll)
     {
         // Collision with Food
-        if (coll.name.StartsWith("Food"))
+        if (coll.CompareTag("Food"))
         {
-            ate = true;
             score++;
             TextScore.text = score.ToString();
+
+            AddTail();
 
             // Destroy the Food
             Destroy(coll.gameObject);
 
-			// And generate again
+            // And generate again
             food.Generate();
         }
-        else if (coll.name.StartsWith("Border") || coll.name.StartsWith("Snake"))
+        if (coll.CompareTag("Border") || coll.CompareTag("Tail"))
         {
-            CancelInvoke("Move");
+            Debug.Log("Game over");
         }
     }
 }
---
+++ Assets/Scripts/Tail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Tail : MonoBehaviour
+{
+    public float Speed;
+
+    public Vector3 tailTarget;
+
+    public Snake mainSnake;
+
+    //public GameObject tailPrefab;
+    public GameObject tailObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainSnake = GameObject.FindGameObjectWithTag("SnakeHead").GetComponent<Snake>();
+        Speed = mainSnake.Speed - 0.5f;
+        tailObj = mainSnake.tail[mainSnake.tail.Count - 2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tailTarget = tailObj.transform.position;
+        transform.LookAt(tailTarget);
+        transform.position = Vector3.Lerp(transform.position, tailTarget, Time.deltaTime * Speed);
+    }
+}
